a7d252fc6aa756a22cf276b114d01faf3af3e659
ichihara <ichihara@glocal-biz.jp>
2025-11-10 17:17:55 +0900

    update

--- app.py
+++ app.py
@@ -269,11 +269,10 @@ def log_search_to_bigquery(_bq_client, keyword, ministries, categories, sub_cate
                 "timestamp": pd.Timestamp.now(tz='Asia/Tokyo').isoformat(),
                 "session_id": st.session_state['user_id'], # ユーザーIDをセッションID代わりに使用
                 "keyword": keyword,
-                # ★★★ 修正: スキーマに合わせ、フィールド名を単数形に変更 (ministry, category, sub_category, year) ★★★
-                "ministry": ", ".join(ministries), 
-                "category": ", ".join(categories),
-                "sub_category": ", ".join(sub_categories),
-                "year": ", ".join([str(y) for y in years]), # リストを文字列に変換
+                "filter_ministries": ", ".join(ministries), 
+                "filter_category": ", ".join(categories),
+                "filter_subcategory": ", ".join(sub_categories),
+                "filter_year": ", ".join([str(y) for y in years]), # リストを文字列に変換
                 "file_count": file_count,
                 "page_count": page_count
             }
